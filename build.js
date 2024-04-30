0f29fdddaab8334b1df120e7b8cd869bd60c5f46
Andreas Zahnen <zahnen@interactive-instruments.de>
2024-04-30 16:27:56 +0200

    packaging

---
+++ build.js
@@ -0,0 +1,61 @@
+import { build } from "esbuild";
+import { clean } from "esbuild-plugin-clean";
+import { replace } from "esbuild-plugin-replace";
+import { copy } from "esbuild-plugin-copy";
+
+const platform = `${process.platform}-${process.arch}`;
+const nodeVersion = process.versions.node.split(".")[0];
+
+if (nodeVersion !== "20") {
+  console.error(
+    `Node.js version ${nodeVersion} is not supported, please use Node.js version 20.`
+  );
+  process.exit(1);
+}
+
+await build({
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  platform: "node",
+  target: "node20",
+  format: "cjs",
+  //logLevel: "debug",
+  /*loader: {
+    ".node": "copy",
+  },*/
+  outdir: "build",
+  outbase: "node_modules",
+  entryNames: "[name]",
+  assetNames: "lib/[dir]/[name]",
+  outExtension: {
+    ".js": ".cjs",
+  },
+  banner: {
+    //js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+    js: "const require2 = require('node:sea').isSea() ? require('node:module').createRequire(__filename) : require;",
+  },
+  plugins: [
+    clean({
+      patterns: ["./build/*"],
+    }),
+    replace({
+      delimiters: ["", ""],
+      values: {
+        "require('../../lib/node-v'":
+          "require2('./lib/@maplibre/maplibre-gl-native/lib/node-v'",
+        "process.versions.modules": `"${process.versions.modules}"`,
+        "${runtimePlatform}": platform,
+        "/mbgl": "/mbgl.node",
+        "if (sharp) {": `sharp = require2('./lib/@img/sharp-${platform}/lib/sharp-${platform}.node'); if (sharp) {`,
+      },
+    }),
+    copy({
+      assets: [
+        {
+          from: ["./node_modules/**/*.node", "./node_modules/**/*.dylib"],
+          to: ["./lib"],
+        },
+      ],
+    }),
+  ],
+});
